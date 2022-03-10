a914ab32d4f4f28850fe810d18a57c70cf980ce0
Ivan Toselli <ivan.toselli31@gmail.com>
2022-03-10 09:38:53 +0100

    Stp 2: Mod 4 var and %

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,31 @@
 /* Variable declaration */
-var userAge;
-var userKm;
-var userRate;
+let userAge;
+let userKm;
+let userRate;
 const rateKm = 0.21; 
+let userRateYoung;
+let userRateOld;
 
 function userData() {
 userAge = prompt("Enter your Age ");
 userKm = prompt("Enter kilometers of your travel ");
+
+/* Calc */
 userRate = rateKm * userKm;
+userRateYoung = ((userRate*20) /100 )
+userRateOld = ((userRate*40) /100 )
+userRateYoung = userRateYoung.toFixed(2);
+userRateOld = userRateOld.toFixed(2);
+
 document.getElementById("ticket").innerHTML =
-`You data: Age: ${userAge} Km: ${userKm} Fare: ${userRate} €`;  
+`You data: Age: ${userAge} Km: ${userKm} Standard Fare: ${userRate} €`;  
     if(userAge < 20){
         document.getElementById("discountYoung").innerHTML =
-        "You are entitled to the under 20 discount.";  
+        `You are entitled to the under 20 discount. Your fare is ${userRateYoung} €`;  
     } 
     else if(userAge > 65){
         document.getElementById("discountOver65").innerHTML =
-        "You are entitled to the over 65 discount.";
+        `You are entitled to the over 65 discount. Your fare is ${userRateOld} €`;
     } 
     else{
         document.getElementById("discountNull").innerHTML =
